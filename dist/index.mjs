ccc25130804385ab99f229e2697af2b0c287f951
staticWagomU <to6wa17@gmail.com>
2024-09-28 00:23:55 +0900

    update

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,11 +1,27 @@
+import { loadDefaultJapaneseParser } from 'budoux';
+
+const WORD_BREAK_STYLE = "word-break:keep-all;overflow-wrap:anywhere;";
+const ZERO_WIDTH_SPACE = "\u200B";
 function markdownItWordBreakStyle(md) {
-  const originalParagraphOpen = md.renderer.rules.paragraph_open || function(tokens, idx, options, _, self) {
-    return self.renderToken(tokens, idx, options);
+  const originalRules = {
+    paragraph_open: md.renderer.rules.paragraph_open || defaultRender,
+    text: md.renderer.rules.text || defaultRender
   };
+  function defaultRender(tokens, idx, options, _, self) {
+    return self.renderToken(tokens, idx, options);
+  }
   md.renderer.rules.paragraph_open = function(tokens, idx, options, env, self) {
     const token = tokens[idx];
-    token.attrSet("style", "word-break:keep-all;overflow-wrap:anywhere");
-    return originalParagraphOpen(tokens, idx, options, env, self);
+    const original_style = token.attrGet("style");
+    let style = original_style && original_style[original_style.length - 1] !== ";" ? original_style + ";" : original_style || "";
+    token.attrSet("style", style + WORD_BREAK_STYLE);
+    return originalRules.paragraph_open(tokens, idx, options, env, self);
+  };
+  md.renderer.rules.text = function(tokens, idx, options, env, self) {
+    const jaParser = loadDefaultJapaneseParser();
+    const processedText = jaParser.parse(tokens[idx].content);
+    tokens[idx].content = processedText.join(ZERO_WIDTH_SPACE);
+    return originalRules.text(tokens, idx, options, env, self);
   };
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,41 @@
 import type MarkdownIt from "markdown-it/index.js";
+import { Renderer, Token } from "markdown-it/index.js";
+import { loadDefaultJapaneseParser } from "budoux";
+
+const WORD_BREAK_STYLE = 'word-break:keep-all;overflow-wrap:anywhere;';
+const ZERO_WIDTH_SPACE = '\u200B';
+
 
 export default function markdownItWordBreakStyle(md: MarkdownIt) {
-  const originalParagraphOpen = md.renderer.rules.paragraph_open || function(tokens, idx, options, _, self) {
-    return self.renderToken(tokens, idx, options);
+
+  const originalRules = {
+    paragraph_open: md.renderer.rules.paragraph_open || defaultRender,
+    text: md.renderer.rules.text || defaultRender
   };
+
+  function defaultRender(tokens: Token[], idx: number, options: MarkdownIt.Options, _: any, self: Renderer) {
+    return self.renderToken(tokens, idx, options);
+  }
+
 
   md.renderer.rules.paragraph_open = function(tokens, idx, options, env, self) {
     const token = tokens[idx];
-    // スタイルを追加
-    token.attrSet('style', 'word-break:keep-all;overflow-wrap:anywhere');
+    const original_style = token.attrGet('style');
+    // original_styleが存在して、最後の文字が';'でない場合、';'を追加
+    let style = original_style && original_style[original_style.length - 1] !== ';' ? original_style + ';' : original_style || '';
 
-    // オリジナルのレンダリング処理を呼び出す
-    return originalParagraphOpen(tokens, idx, options, env, self);
+    token.attrSet('style', style + WORD_BREAK_STYLE);
+    return originalRules.paragraph_open(tokens, idx, options, env, self);
+  };
+
+  md.renderer.rules.text = function(tokens, idx, options, env, self) {
+    const jaParser = loadDefaultJapaneseParser();
+    const processedText = jaParser.parse(tokens[idx].content);
+
+
+    // 処理されたテキストでトークンの内容を置き換え
+    tokens[idx].content = processedText.join(ZERO_WIDTH_SPACE);
+    return originalRules.text(tokens, idx, options, env, self);
   };
 
 }
